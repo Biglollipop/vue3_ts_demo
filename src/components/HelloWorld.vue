94b517d4356ffd7b270e3e30b4ae0160e87a9110
chenshiqiang <chenshiqiang@inspur.com>
2022-01-25 23:54:59 +0800

    🎉 init...

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-button type="primary" @click="submitHandle">提交</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineExpose, defineProps, defineEmits } from 'vue'
+const props = defineProps<{
+  msg:string
+}>()
+// eslint-disable-next-line func-call-spacing
+const emit = defineEmits<{
+  (e:'clickHandle', msg:string):void
+}>()
+const submitHandle = () => {
+  emit('clickHandle', props.msg)
+}
+defineExpose({
+  props
+})
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld @click-handle="clickHandle" msg="Welcome to Your Vue.js + TypeScript App"/>
+    <span>{{msg}}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+const msg = ref <string>('')
+const clickHandle = (val:string) => {
+  console.log('val ', val)
+  msg.value = val
+}
+</script>
